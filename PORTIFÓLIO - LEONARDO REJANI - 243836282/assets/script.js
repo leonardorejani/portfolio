3215dac68ba367d17d9ad5b3e4450bf57ca17884
Leonardo Rejani <leodevrejani@gmail.com>
2025-03-17 20:24:19 -0300

    Add files via upload

---
+++ PORTIFÓLIO - LEONARDO REJANI - 243836282/assets/script.js
@@ -0,0 +1,50 @@
+window.addEventListener('scroll', function() {
+    const nav = document.querySelector('.nav');
+    if (window.scrollY > 50) {
+        nav.classList.add('scrolled');
+    } else {
+        nav.classList.remove('scrolled');
+    }
+});
+
+let currentSlide = 0;
+
+function showSlide(index) {
+    const slides = document.querySelectorAll('.carousel-item');
+    if (index >= slides.length) {
+        currentSlide = 0;
+    } else if (index < 0) {
+        currentSlide = slides.length - 1;
+    } else {
+        currentSlide = index;
+    }
+    const offset = -currentSlide * 100;
+    document.querySelector('.carousel-inner').style.transform = `translateX(${offset}%)`;
+}
+
+function nextSlide() {
+    showSlide(currentSlide + 1);
+}
+
+function prevSlide() {
+    showSlide(currentSlide - 1);
+}
+
+setInterval(() => {
+    nextSlide();
+}, 4000);
+
+// Função do Menu Âncora
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(event) {
+        event.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        if (targetElement) {
+            window.scrollTo({
+                top: targetElement.offsetTop - 50, // Ajuste para compensar a altura do menu fixo, se necessário
+                behavior: 'smooth'
+            });
+        }
+    });
+});
